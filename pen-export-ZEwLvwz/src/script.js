1d2156eb45250fbf2726b13403f8e1fe611c3321
Jona Robin <51392525+jonarobin@users.noreply.github.com>
2023-11-17 00:10:55 -0300

    Update script.js

--- pen-export-ZEwLvwz/src/script.js
+++ pen-export-ZEwLvwz/src/script.js
@@ -1,108 +1,103 @@
-// Define una función para inicializar el mapa
-function initializeMap() {
-    let map = L.map('map');
+let map = L.map('map');
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd',
-        maxZoom: 20,
-        ext: 'png'
-    }).addTo(map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 20,
+    ext: 'png'
+}).addTo(map);
 
-    let geojson_url = "https://raw.githubusercontent.com/jonarobin/Map-demo/main/Demo%2016%20nov.geojson";
+let geojson_url = "https://raw.githubusercontent.com/jonarobin/Map-demo/main/Demo%2016%20nov.geojson";
 
-    let markersLayer;
-    let allCategories = []; // Array que almacena todas las categorías
+let markersLayer;
+let allCategories = []; // Array que almacena todas las categorías
 
-    fetch(geojson_url)
-        .then(res => res.json())
-        .then(data => {
-            markersLayer = L.geoJson(data, {
-                pointToLayer: function (feature, latlng) {
-                    allCategories = allCategories.concat(feature.properties.Rubro);
-                    return L.marker(latlng, {
-                        icon: L.divIcon({
-                            className: 'custom-marker',
-                            html: `<div class="marker-content"><img class="marker-image" src="${feature.properties.Foto}" alt="${feature.properties.Nombre}" /></div>`
-                        })
-                    });
-                },
-                onEachFeature: function (feature, layer) {
-                    layer.on('click', function () {
-                        window.open(feature.properties['URL Store'], '_blank');
-                    });
-                }
-            });
+fetch(geojson_url)
+    .then(res => res.json())
+    .then(data => {
+        markersLayer = L.geoJson(data, {
+            pointToLayer: function (feature, latlng) {
+                allCategories = allCategories.concat(feature.properties.Rubro); // Agrega las categorías al array
 
-            markersLayer.addTo(map);
-
-            map.fitBounds(markersLayer.getBounds());
+                return L.marker(latlng, {
+                    icon: L.divIcon({
+                        className: 'custom-marker',
+                        html: `<div class="marker-content"><img class="marker-image" src="${feature.properties.Foto}" alt="${feature.properties.Nombre}" /></div>`
+                    })
+                });
+            },
+            onEachFeature: function (feature, layer) {
+                layer.on('click', function () {
+                    window.open(feature.properties['URL Store'], '_blank');
+                });
+            }
         });
 
-    let searchInput = document.getElementById('searchInput');
-    const geocoder = L.Control.Geocoder.nominatim({
-        geocodingQueryParams: {
-            viewbox: '-58.5664,-34.7059,-58.3413,-34.5283',
-            bounded: 1,
-            countrycodes: 'AR',
-            limit: 5,
-        },
+        markersLayer.addTo(map);
+
+        map.fitBounds(markersLayer.getBounds());
     });
 
-    searchInput.addEventListener('input', function () {
-        let searchText = searchInput.value;
-        if (searchText) {
-            geocoder.geocode(searchText, function (results) {
-                if (results.length > 0) {
-                    let latlng = results[0].center;
-                    map.setView(latlng, 15);
-                }
-            });
+let searchInput = document.getElementById('searchInput');
+const geocoder = L.Control.Geocoder.nominatim({
+    geocodingQueryParams: {
+        viewbox: '-58.5664,-34.7059,-58.3413,-34.5283',
+        bounded: 1,
+        countrycodes: 'AR',
+        limit: 5,
+    },
+});
+
+searchInput.addEventListener('input', function () {
+    let searchText = searchInput.value;
+    if (searchText) {
+        geocoder.geocode(searchText, function (results) {
+            if (results.length > 0) {
+                let latlng = results[0].center;
+                map.setView(latlng, 15);
+            }
+        });
+    }
+});
+
+let categoriaDropdown = document.getElementById('categoriaDropdown');
+categoriaDropdown.addEventListener('change', function () {
+    let selectedCategoria = categoriaDropdown.value;
+
+    // Oculta todos los marcadores
+    markersLayer.eachLayer(function (layer) {
+        layer._icon.classList.add('hidden');
+    });
+
+    // Muestra solo los marcadores que cumplen con la categoría seleccionada
+    markersLayer.eachLayer(function (layer) {
+        let rubros = layer.feature.properties.Rubro;
+        if (selectedCategoria === '' || rubros.includes(selectedCategoria)) {
+            layer._icon.classList.remove('hidden');
+        }
+    });
+});
+
+let changeCategoryButton = document.getElementById('changeCategoryButton');
+let currentCategoryIndex = 0;
+
+changeCategoryButton.addEventListener('click', function () {
+    currentCategoryIndex = (currentCategoryIndex + 1) % allCategories.length;
+    let selectedCategoria = allCategories[currentCategoryIndex];
+
+    // Oculta todos los marcadores
+    markersLayer.eachLayer(function (layer) {
+        layer._icon.classList.add('hidden');
+    });
+
+    // Muestra solo los marcadores que cumplen con la categoría seleccionada
+    markersLayer.eachLayer(function (layer) {
+        let rubros = layer.feature.properties.Rubro;
+        if (selectedCategoria === '' || rubros.includes(selectedCategoria)) {
+            layer._icon.classList.remove('hidden');
         }
     });
 
-    let categoriaDropdown = document.getElementById('categoriaDropdown');
-    categoriaDropdown.addEventListener('change', function () {
-        let selectedCategoria = categoriaDropdown.value;
-
-        // Oculta todos los marcadores
-        markersLayer.eachLayer(function (layer) {
-            layer._icon.classList.add('hidden');
-        });
-
-        // Muestra solo los marcadores que cumplen con la categoría seleccionada
-        markersLayer.eachLayer(function (layer) {
-            let rubros = layer.feature.properties.Rubro;
-            if (selectedCategoria === '' || rubros.includes(selectedCategoria)) {
-                layer._icon.classList.remove('hidden');
-            }
-        });
-    });
-
-    let changeCategoryButton = document.getElementById('changeCategoryButton');
-    let currentCategoryIndex = 0;
-
-    changeCategoryButton.addEventListener('click', function () {
-        currentCategoryIndex = (currentCategoryIndex + 1) % allCategories.length;
-        let selectedCategoria = allCategories[currentCategoryIndex];
-
-        // Oculta todos los marcadores
-        markersLayer.eachLayer(function (layer) {
-            layer._icon.classList.add('hidden');
-        });
-
-        // Muestra solo los marcadores que cumplen con la categoría seleccionada
-        markersLayer.eachLayer(function (layer) {
-            let rubros = layer.feature.properties.Rubro;
-            if (selectedCategoria === '' || rubros.includes(selectedCategoria)) {
-                layer._icon.classList.remove('hidden');
-            }
-        });
-
-        // Actualiza el dropdown
-        categoriaDropdown.value = selectedCategoria;
-    });
-}
-
-// Registra la función para ejecutarse después de que la página haya terminado de cargar
-window.onload = initializeMap;
+    // Actualiza el dropdown
+    categoriaDropdown.value = selectedCategoria;
+});
