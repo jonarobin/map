364299189c104e5466fd1d33083d6f747d7c5065
Jona Robin <51392525+jonarobin@users.noreply.github.com>
2023-11-27 23:39:10 -0300

    Update script.js

--- mapaBronceado/script.js
+++ mapaBronceado/script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     ext: 'png'
 }).addTo(map);
 
-let geojsonUrl = "ttps://raw.githubusercontent.com/jonarobin/map/main/geojson/bronceado.geojson";
+let geojsonUrl = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/bronceado.geojson";
 
 let markersLayer;
 let allCategories = [];
