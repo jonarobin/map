5e8d8b61bdc64a493957b264a8ddb43307c487cb
Jona Robin <51392525+jonarobin@users.noreply.github.com>
2023-11-27 23:46:38 -0300

    Update script.js

--- mapaPeluqueria/script.js
+++ mapaPeluqueria/script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     ext: 'png'
 }).addTo(map);
 
-let geojson_url = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/peluqueria.geojson";
+let geojsonUrl = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/peluqueria.geojson";
 
 let markersLayer;
 let allCategories = [];
