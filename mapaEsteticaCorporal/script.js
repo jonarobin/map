eaaed26cb594823afbe024f818fd2a7466839fc4
Jona Robin <51392525+jonarobin@users.noreply.github.com>
2023-11-27 23:40:46 -0300

    Update script.js

--- mapaEsteticaCorporal/script.js
+++ mapaEsteticaCorporal/script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     ext: 'png'
 }).addTo(map);
 
-let geojson_url = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/esteticaCorporal.geojson";
+let geojsonUrl = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/esteticaCorporal.geojson";
 
 let markersLayer;
 let allCategories = [];
