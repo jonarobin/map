e51b620f2a05807e7ee51e881be5fa8da172a73d
Jona Robin <51392525+jonarobin@users.noreply.github.com>
2023-12-14 22:29:23 -0300

    Update script.js

--- mapaMasajes/script.js
+++ mapaMasajes/script.js
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r
     ext: 'png'
 }).addTo(map);
 
-let geojson_Url = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/masajes.geojson";
+let geojsonUrl = "https://raw.githubusercontent.com/jonarobin/map/main/geojson/bronceado.geojson";
 
 let markersLayer;
 let allCategories = [];
